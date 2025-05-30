58a208c222637448b12789c3da54ffac1461bca4
Italo Braz <italo.brazz@outlook.com>
2025-05-29 21:00:19 -0300

    modificando rotas - Guilherme Silva

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/auth');
+const { auth } = require('../middlewares/auth');
 const userController = require('../controllers/userController');
 
 router.put('/:id', auth, userController.atualizarUsuario);
